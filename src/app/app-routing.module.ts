1ff60abdd0f33e3248ec2d837e8ef133e79bcafe
David Rojas <david154x@hotmail.com>
2023-04-27 20:59:04 -0500

    Revert "Merge branch 'Sepi' of https://github.com/david154x/RSavillaSasBic into test"
    This reverts commit 7de8ca80f025c79789fb62df4d01c973b613ddde, reversing
    changes made to f6ae5b942a81c3c3f9d32a0d48f32702680190af.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkComponent } from './work/work.component';
 import { HomeComponent } from './home/home.component';
-// HEAD
-import { ManagementComponent } from './management/management.component';
-
 import { NavigationComponent } from './navigation/navigation.component';
-// ae51a11fbbb7a8052cebc191cf587b42db63ad6f
 
 
 
 const routes: Routes = [
   // Agregar nuevas rutas y redireccionar al Home
-// HEAD
-  { path: 'work', component: WorkComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'management', component:  ManagementComponent },
-////////////////////77
   {
     path: '',
     component: NavigationComponent,
@@ -26,23 +17,11 @@ const routes: Routes = [
       { path: "**", redirectTo: "home" }
     ]
   },
-// ae51a11fbbb7a8052cebc191cf587b42db63ad6f
   { path: '**', redirectTo: 'home'},
 ];
-
-@NgModule({
-  imports: [
-
-
-  ],
-
-})
-export class AppModule { }
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,3 @@
-import { MenuModule } from 'primeng/menu';
-import { CalendarModule } from 'primeng/calendar';
-import { PanelModule } from 'primeng/panel';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,11 +8,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { WorkComponent } from './work/work.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { NavigationComponent } from './navigation/navigation.component';
-import { ManagementComponent } from './management/management.component';
-
-
 
 
 
@@ -27,19 +20,12 @@ import { ManagementComponent } from './management/management.component';
     FooterComponent,
     HomeComponent,
     NavigationComponent,
-    ManagementComponent,
-
   ],
   imports: [
     BrowserModule,
     PrimengModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PanelModule,
-    ScrollPanelModule,
-    MenuModule,
-    CalendarModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
