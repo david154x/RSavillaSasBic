88324a2c87c7698552197fca6f00db052b7dfe1a
David Rojas <david154x@hotmail.com>
2023-04-27 22:13:14 -0500

    awa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { WorkComponent } from './work/work.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ManagementComponent } from './management/management.component';
 
 
 
 const routes: Routes = [
   // Agregar nuevas rutas y redireccionar al Home
   {
-    path: '',
-    component: NavigationComponent,
+    path: '',  component: NavigationComponent,
     children: [
       { path: 'work', component: WorkComponent },
       { path: 'home', component: HomeComponent },
-      { path: "**", redirectTo: "home" }
+      { path: "**", redirectTo: "home" },
+      { path: 'management', component:  ManagementComponent },
     ]
   },
   { path: '**', redirectTo: 'home'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PanelModule } from 'primeng/panel';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './primeng/primeng.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +10,7 @@ import { WorkComponent } from './work/work.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
-
+import { ManagementComponent } from './management/management.component';
 
 
 @NgModule({
@@ -20,14 +21,16 @@ import { NavigationComponent } from './navigation/navigation.component';
     FooterComponent,
     HomeComponent,
     NavigationComponent,
+    ManagementComponent,
   ],
   imports: [
     BrowserModule,
     PrimengModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    PanelModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
